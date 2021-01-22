0357cff0b403f11b92a59a0d172cbae0e639850a
Joanna93-JJ <68261251+Joanna93-JJ@users.noreply.github.com>
2021-01-22 17:29:59 +0800

    修改fetch方法后，出现window错误

--- framework/bigcommerce/api/operations/get-product.ts
+++ framework/bigcommerce/api/operations/get-product.ts
@@ -2,7 +2,9 @@ import type { GetProductQuery, GetProductQueryVariables } from '../../schema'
 import setProductLocaleMeta from '../utils/set-product-locale-meta'
 import { productInfoFragment } from '../fragments/product'
 import { BigcommerceConfig, getConfig } from '..'
-import fetchSteedosGraphqlApi from '../utils/fetch-steedos-api'
+import { fetchSteedosGraphqlApi } from '../utils/fetch-steedos-api'
+import { convertObjType, convertPriceType } from './type-convert'
+
 
 export const getProductQuery = /* GraphQL */ `
   query getProduct(
@@ -106,12 +108,59 @@ async function getProduct({
   const { data } = await config.fetch<GetProductQuery>(query, { variables })
   const product = data.site?.route?.node
 
+
   //调用steedos的fetch，通过graphql获取数据
-  const product2  = await fetchSteedosGraphqlApi()
-  console.log('product2---', product2.data)
+  const proQuery = `
+        query{
+            node:cc_product__c{
+                __typename
+                _id
+                name
+                short_desc__c:description
+                related__cc_product_category__c {
+                  _id
+                  name
+                }
+                related__cc_product_spec__c {
+                  _id
+                  name
+                  spec_value__c
+                }
+                related__cc_product_media__c{
+                  _id
+                  name
+                  url__c
+                }
+            }          
+        }
+    `
+  const priceQuery = `
+    query{
+      cc_price_list__c {
+        __typename
+        _id
+        name
+        related__cc_price_list_items__c {
+          _id
+          name
+          price_value__c
+        }
+      }
+    }
+  `
+  
+  const product_main  = await fetchSteedosGraphqlApi(proQuery)
+    console.log('product_main--', product_main)
+
+  const price_list = await fetchSteedosGraphqlApi(priceQuery)
+ 
   //TODO convert
+  const product_all = convertObjType(product_main)
+  const price_all = convertPriceType(price_list)
+  
 
-
+  console.log('product_new---', product_all)
+  console.log('product_price---', price_all)
   if (product?.__typename === 'Product') {
     if (locale && config.applyLocale) {
       setProductLocaleMeta(product)
--- framework/bigcommerce/api/operations/type-convert.ts
+++ framework/bigcommerce/api/operations/type-convert.ts
@@ -1,1 +1,45 @@
 //TODO  convert Product json
+
+export function convertObjType(objJson:any){
+
+    let productObj = {
+        site:{
+            route:{
+                node:{
+
+                }
+            }
+        }
+    } 
+
+    productObj.site.route.node = objJson.data
+
+    return productObj
+
+}
+
+export function convertPriceType(objJson:any){
+
+    let priceObj = {
+        price :{},
+        salePrice:{},
+        retailPrice:{}
+    }
+    if(objJson.data){
+        priceObj.price = objJson.data[0]
+        priceObj.salePrice = objJson.data[1] || null
+        priceObj.retailPrice = objJson.data[2] || null
+    }
+
+    return priceObj
+}
+
+
+// "prices": {
+//     "price": {
+//         "value": 25,
+//         "currencyCode": "USD"
+//     },
+//     "salePrice": null,
+//     "retailPrice": null
+// }
